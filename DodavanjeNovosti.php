7c7d6332a30ae6ab71102f92534f6d301afdcc5e
Ilvana <ibrankovic1@etf.unsa.ba>
2015-05-13 21:29:26 +0200

    Brisane

--- DodavanjeNovosti.php
+++
@@ -1,9 +0,0 @@
-<?php
-//header('Content-Type: text/html; charset=utf-8');
-$svitxt=glob("novosti/*.txt");
-foreach ($svitxt as $key) {
-	$dat=$key;
-	$html="novakolekcija.html";
-	include "DodajDioKodNovosti.php";
-}
-?>
