aaf2eefd9acf14f0edf5be284b5cf761912b4264
Ilvana <ibrankovic1@etf.unsa.ba>
2015-06-09 20:25:37 +0200

    jksajsajk
    sajhjshjsjh

--- PrikazPrijavljenogKorisnika.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-?>
-<?php 
-$a = htmlEntities($_SESSION['username'], ENT_QUOTES);
-echo 'Prijavljeni ste sa korisničkim imenom:'.$a.'<input type="submit"  id="OdjavaDugme" value="Odjava">';
-?> 
